256de9e69323dad146ca27fe4409baf09fba4357
itsrafaelapaes <paesrafaela49@gmail.com>
2026-09-17 12:50:20 -0300

    adicionando estrutura de decisao

--- calcular.php
+++ calcular.php
@@ -7,6 +7,14 @@ $n2= (float)$_POST['n2'];
 $adicao=$n1+$n2;
 $subtracao = $n1 + $n2;
 $multiplicacao = $n1 * $n2;
+
+$divisaoValida = $n2 !=0; //Atribuir o resultado logico na variavel
+if ($divisaoValida==true)
+{
+    $divisao=$n1/$n2;
+    $modulo=$n1%$n2;
+}
+
 $divisao = $n1 / $n2;
 $modulo = $n1 % $n2;
 $potencia = $n1 ** $n2;
@@ -37,7 +45,18 @@ $concatenacao = $n1.$n2; //o ponto ele JUNTA e texto, não soma como número
             <?php echo "$n1 x $n2 = $multiplicacao"; ?>
         </h3>
         <h3>
-            <?php echo "$n1 % $n2 = $divisao";?>
+            <?php
+            if ($divisaoValida ==true)
+            {
+                echo "$n1 % $n2 = $divisao";
+                echo "<br>";
+                echo "$n1 modulo $n2 = ".number_format($modulo,2, ',',',');
+            }
+           else
+           {
+            echo "<h1>Não há divisão por zero!</h1>";
+           }
+           ?>
         </h3>
         <h3>
             <?php echo "$n1<sup> $n2 </sup>= $potencia"; ?>
