adedd483916578ab132b3256bbebd8693823d191
itsrafaelapaes <paesrafaela49@gmail.com>
2026-09-17 11:56:09 -0300

    operacoes

--- calcular.php
+++ calcular.php
@@ -1,22 +1,17 @@
 <?php
-//OBTER OS DADOS (ENTRADA)
-$Nome = $_POST['tnome'];
-$anoNasc = (int) $_POST['tano'];
-$anoAtual = (int) date('Y'); //Obtem o ano do sistema
+//ENTRADA
+$n1= (float)$_POST['n1'];
+$n2= (float)$_POST['n2'];
 
 //PROCESSAMENTO
-$Idade =$anoAtual - $anoNasc;
+$adicao=$n1+$n2;
+$subtracao = $n1 + $n2;
+$multiplicacao = $n1 * $n2;
+$divisao = $n1 / $n2;
+$modulo = $n1 % $n2;
+$potencia = $n1 ** $n2;
+$concatenacao = $n1.$n2; //o ponto ele JUNTA e texto, não soma como número
 
-$dias = $Idade* 365.25;
-$horas = $dias* 24;
-$minutos = $horas* 60;
-
-$batimentos = $minutos* 75;
-$respiracoes = $minutos* 17;
-$bat_restantes = (90 * 365.25 * 24 * 60 * 75) - $batimentos;
-
-//SAIDA
-echo "<h2> $Nome, sua idade é $Idade anos</h2>";
 ?>
 
 <!DOCTYPE html>
@@ -24,18 +19,36 @@ echo "<h2> $Nome, sua idade é $Idade anos</h2>";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tempo de vida</title>
+    <title>Operações aritméticas</title>
+    
 </head>
 <body>
-    <h1>Tempo de vida</h1>
-    <hr>
-    <h2><?php echo "<h2>$Nome sua idade é: $Idade</h2>"; ?>
-    <h2><?php echo "<h2>$Nome voce viveu: $dias dias</h2>"; ?>
-    <h2><?php echo "<h2>$Nome voce viveu: $horas horas</h2>"; ?>
-    <h2><?php echo "<h2>$Nome voce viveu: $minutos minutos</h2>"; ?>
-    <h2><?php echo "<h2>$Nome voce teve: $batimentos batimentos no total</h2>"; ?>
-    <h2><?php echo "<h2>$Nome voce teve: $respiracoes respiracoes no total</h2>"; ?>
-    <h2><?php echo "<h2>$Nome voce tem $bat_restantes de batimentos restantes </h2>"; ?>
+    <div class= "card">
+        <h1>Operações Aritméticas</h1>
+        <h1>Resultados</h1>
+        <hr>
+        <h3>
+            <?php echo "$n1 + $n2 = $adicao"; ?>
+        </h3>
+        <h3>
+            <?php echo "$n1 - $n2 = $subtracao"; ?>
+        </h3>
+        <h3>
+            <?php echo "$n1 x $n2 = $multiplicacao"; ?>
+        </h3>
+        <h3>
+            <?php echo "$n1 % $n2 = $divisao";?>
+        </h3>
+        <h3>
+            <?php echo "$n1<sup> $n2 </sup>= $potencia"; ?>
+        </h3>
+        <h3>
+            <?php echo "$n1 | $n2 = $concatenacao"; ?>
+        </h3>
 
+    <a href="index.php">voltar</a>
+       
+    <div>
+    
 </body>
 </html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operações aritméticas</title>
+    
+</head>
+<body>
+    <div>
+        <h1>Operações Aritméticas</h1>
+        <hr>
+        <form action="calcular.php" method="post">
+        <p> 
+            <label for="n1">
+            Informe um número:
+            </label>
+            <input type="number" name="n1" id="n1" required>
+        </p>
+        <p> 
+            <label for="n1">
+            Informe outro número
+            </label>
+            <input type="number" name="n2" id="n2" required>
+        </p>
+        <p>
+            <input type="submit" value="calcular">
+            <input type="reset" value="limpar">
+        </p>
+    <div>
+    
+</body>
+</html>
